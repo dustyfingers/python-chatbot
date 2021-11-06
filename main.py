a65b2b367d772231a6e2361f510937c8ef220ff2
lou <dustyfingers01257@gmail.com>
2021-11-06 12:13:21 -0600

    stemmed words and sorted words and labels, created 'word bag'

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import nltk 
 nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
-import numpy
+import numpy as np
 import tflearn
 import tensorflow
 import random
@@ -14,7 +14,8 @@ with open('intents.json') as file:
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data['intents']:
     for pattern in intent['patterns']:
@@ -22,7 +23,41 @@ for intent in data['intents']:
         wrds = nltk.word_tokenize(pattern)
         # put wrds into words array from above
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
 
         if intent['tag'] not in labels:
             labels.append(intent['tag'])
+
+
+# stem words, remove dupes etc
+words = [stemmer.stem(w.lower()) for w in words]
+# set() removes dupes, list() converts back to list, sorted() sorts the items
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for wrd in wrds:
+        if wrd in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = np.array(training)
+output = np.array(output)
